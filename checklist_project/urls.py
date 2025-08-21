27de95b216e2a8ac54bf1f58da2fa17b40531f73
pedro-dias12 <pedrohenriquecardosodias2007@gmail.com>
2025-08-21 01:54:59 -0300

    Resolvendo conflitos com a main

--- checklist_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # 👈 import necessário
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('check/', include('check.urls')),  # 👈 adiciona as rotas do app "check"
-]
